dcdfe5e8cf825e3508ddbfa055a3984a27aa62ff
Luiggi Reffatti <luiggi@firasoft.com>
2023-01-10 11:41:45 -0300

    Style changes: Indentation, {} and ;

--- ext/js/background.js
+++ ext/js/background.js
@@ -1,62 +1,74 @@
-chrome.commands.onCommand.addListener((command) => {
-  handleCommand(command)
-})
+chrome.commands.onCommand.addListener((command) => 
+{
+	handleCommand(command);
+});
 
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
-    if (request.hasOwnProperty('message')) {
-      setIcon(request.message)
-    }
-  })
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => 
+{
+	if (request.hasOwnProperty('message'))
+	  setIcon(request.message);
+});
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  handleCommand('toggle_mute')
-})
+chrome.browserAction.onClicked.addListener((tab) => 
+{
+	handleCommand('toggle_mute');
+});
 
-function handleCommand(command) {
-  chrome.windows.getAll({ populate: true }, windowList => {
-    let gatherTabs = getGatherTabs(windowList)
+function handleCommand(command) 
+{
+	chrome.windows.getAll({ populate: true }, windowList => 
+	{
+		let gatherTabs = getGatherTabs(windowList);
 
-    if (gatherTabs.length > 0) {
-      processCommand(command, gatherTabs)
-    }
-  })
+		if (gatherTabs.length > 0)
+		  processCommand(command, gatherTabs);
+	});
 }
 
-function getGatherTabs(windowList) {
-  let gatherTabs = []
-  windowList.forEach(w => {
-    w.tabs.forEach(tab => {
-      if (tab && tab.url && tab.url.startsWith('https://app.gather.town/')) {
-        gatherTabs.push(tab)
-      }
-    })
-  })
-  return gatherTabs
+function getGatherTabs(windowList) 
+{
+	let gatherTabs = [];
+	windowList.forEach(w => 
+	{
+		w.tabs.forEach(tab => 
+		{
+			if (tab && tab.url && tab.url.startsWith('https://app.gather.town/')) 
+				gatherTabs.push(tab);
+		});
+	});
+	return gatherTabs;
 }
 
-function processCommand(command, gatherTabs) {
-  gatherTabs.forEach((tab) => {
-    chrome.tabs.sendMessage(tab.id, { command: command }, (response) => {
-      setIcon(response.message)
-    })
-  })
+function processCommand(command, gatherTabs) 
+{
+	gatherTabs.forEach((tab) => 
+	{
+		chrome.tabs.sendMessage(tab.id, { command: command }, (response) => 
+		{
+			setIcon(response.message);
+		});
+	});
 }
 
-function setIcon(status) {
-  let iconType = ''
-  if (status === 'muted' || status === 'unmuted') {
-    iconType = '_' + status
-  }
-  let title = status.charAt(0).toUpperCase() + status.substr(1)
-  chrome.browserAction.setIcon({
-    path: {
-      "32": `icons/icon32${ iconType }.png`,
-      "48": `icons/icon48${ iconType }.png`,
-      "128": `icons/icon128${ iconType }.png`
-    }
-  })
-  chrome.browserAction.setTitle({
-    title: title
-  })
+function setIcon(status) 
+{
+	let iconType = '';
+	if (status === 'muted' || status === 'unmuted') 
+		iconType = '_' + status;
+
+	let title = status.charAt(0).toUpperCase() + status.substr(1);
+	chrome.browserAction.setIcon(
+	{
+		path: 
+		{
+			"32": `icons/icon32${ iconType }.png`,
+			"48": `icons/icon48${ iconType }.png`,
+			"128": `icons/icon128${ iconType }.png`
+		}
+	});
+	
+	chrome.browserAction.setTitle(
+	{
+		title: title
+	});
 }
--- ext/js/gathermute.js
+++ ext/js/gathermute.js
@@ -3,130 +3,154 @@
 // Active: class="css-15e33lp", Muted: class="css-oau38", 
 const MUTE_BUTTON = '[role="button"][aria-label][data-is-muted]'
 
-const waitUntilElementExists = (DOMSelector, MAX_TIME = 5000) => {
-  let timeout = 0
+const waitUntilElementExists = (DOMSelector, MAX_TIME = 5000) => 
+{
+	let timeout = 0;
 
-  const waitForContainerElement = (resolve, reject) => {
-    const container = document.querySelector(DOMSelector)
-    timeout += 100
+	const waitForContainerElement = (resolve, reject) => 
+	{
+		const container = document.querySelector(DOMSelector);
+		timeout += 100;
 
-    if (timeout >= MAX_TIME) reject('Element not found')
+		if (timeout >= MAX_TIME) 
+			reject('Element not found');
 
-    if (!container || container.length === 0) {
-      setTimeout(waitForContainerElement.bind(this, resolve, reject), 100)
-    } else {
-      resolve(container)
-    }
-  }
+		if (!container || container.length === 0) 
+			setTimeout(waitForContainerElement.bind(this, resolve, reject), 100);
+		else 
+			resolve(container);
+	}
 
-  return new Promise((resolve, reject) => {
-    waitForContainerElement(resolve, reject);
-  })
+	return new Promise((resolve, reject) => 
+	{
+		waitForContainerElement(resolve, reject);
+	});
 }
 
-var waitingForMuteButton = false
+var waitingForMuteButton = false;
 
-function waitForMuteButton() {
-  if (waitingForMuteButton) {
-    return
-  }
-  waitingForMuteButton = true
-  waitUntilElementExists(MUTE_BUTTON)
-    .then((el) => {
-      waitingForMuteButton = false
-      updateMuted()
-      watchIsMuted(el)
-    })
-    .catch((error) => {
-      chrome.extension.sendMessage({ message: 'disconnected' })
-    })
+function waitForMuteButton() 
+{
+	if (waitingForMuteButton) 
+		return;
+
+	waitingForMuteButton = true;
+	waitUntilElementExists(MUTE_BUTTON);
+    .then((el) => 
+	{
+		waitingForMuteButton = false
+		updateMuted()
+		watchIsMuted(el)
+    });
+    .catch((error) => 
+	{
+		chrome.extension.sendMessage({ message: 'disconnected' });
+    });
 }
 
-var muted = false
+var muted = false;
 
 //TODO: This will need to be done using the class in the button.
-function isMuted() {
-  let dataIsMuted = document.querySelector(MUTE_BUTTON)
-      .getAttribute('data-is-muted')
-  return dataIsMuted == 'true'
+function isMuted() 
+{
+	let dataIsMuted = document.querySelector(MUTE_BUTTON)
+		.getAttribute('data-is-muted');
+	return dataIsMuted == 'true';
 }
 
-function updateMuted(newValue) {
-  muted = newValue || isMuted()
-  chrome.extension.sendMessage({ message: muted ? 'muted' : 'unmuted' })
+function updateMuted(newValue) 
+{
+	muted = newValue || isMuted();
+	chrome.extension.sendMessage({ message: muted ? 'muted' : 'unmuted' });
 }
 
-var isMutedObserver
+var isMutedObserver;
 
-function watchIsMuted(el) {
-  if (isMutedObserver) {
-    isMutedObserver.disconnect()
-  }
-  //TODO: This will need to be done using the class in the button.
-  isMutedObserver = new MutationObserver((mutations) => {
-    let newValue = mutations[0].target.getAttribute('data-is-muted') == 'true'
+function watchIsMuted(el) 
+{
+	if (isMutedObserver) 
+		isMutedObserver.disconnect();
 
-    if (newValue != muted) {
-      updateMuted(newValue)
-    }
-  })
-  isMutedObserver.observe(el, {
-    attributes: true,
-    attributeFilter: ['data-is-muted']
-  })
+	//TODO: This will need to be done using the class in the button.
+	isMutedObserver = new MutationObserver((mutations) => 
+	{
+		let newValue = mutations[0].target.getAttribute('data-is-muted') == 'true';
+
+		if (newValue != muted) 
+			updateMuted(newValue);
+	});
+	
+	isMutedObserver.observe(el, 
+	{
+		attributes: true,
+		attributeFilter: ['data-is-muted']
+	});
 }
 
-function watchBodyClass() {
-  const bodyClassObserver = new MutationObserver((mutations) => {
-    let newClass = mutations[0].target.getAttribute('class')
-    if (mutations[0].oldValue != newClass) {
-      waitForMuteButton()
-    }
-  })
-  bodyClassObserver.observe(document.querySelector('body'), {
-    attributes: true,
-    attributeFilter: ['class'],
-    attributeOldValue: true
-  })
+function watchBodyClass() 
+{
+	const bodyClassObserver = new MutationObserver((mutations) => 
+	{
+		let newClass = mutations[0].target.getAttribute('class');
+		if (mutations[0].oldValue != newClass) 
+			waitForMuteButton();
+	});
+	
+	bodyClassObserver.observe(document.querySelector('body'), 
+	{
+		attributes: true,
+		attributeFilter: ['class'],
+		attributeOldValue: true
+	});
 }
 
-watchBodyClass()
+watchBodyClass();
 
-window.onbeforeunload = (event) => {
-  chrome.extension.sendMessage({ message: 'disconnected' })
+window.onbeforeunload = (event) => 
+{
+  chrome.extension.sendMessage({ message: 'disconnected' });
 }
 
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
-    muted = isMuted()
-    if (request && request.command && request.command === 'toggle_mute') {
-      muted = !muted
-      sendKeyboardCommand()
-    } else if (request && request.command && request.command === 'mute') {
-      if (!muted) {
-        muted = !muted
-        sendKeyboardCommand()
-      }
-    } else if (request && request.command && request.command === 'unmute') {
-      if (muted) {
-        muted = !muted
-        sendKeyboardCommand()
-      }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => 
+{
+    muted = isMuted();
+    if (request && request.command && request.command === 'toggle_mute') 
+	{
+		muted = !muted;
+		sendKeyboardCommand();
+    } 
+	else if (request && request.command && request.command === 'mute') 
+	{
+		if (!muted) 
+		{
+			muted = !muted;
+			sendKeyboardCommand();
+		}
+    } 
+	else if (request && request.command && request.command === 'unmute') 
+	{
+		if (muted) 
+		{
+			muted = !muted;
+			sendKeyboardCommand();
+		}
     }
 
     sendResponse({ message: muted ? 'muted' : 'unmuted' });
-  })
+});
 
 //TODO: Fix this key to be ctrl+shift+A
-const keydownEvent = new KeyboardEvent('keydown', {
-  "key": "d",
-  "code": "KeyD",
-  "metaKey": true,
-  "charCode": 100,
-  "keyCode": 100,
-  "which": 100
-})
+const keydownEvent = new KeyboardEvent('keydown', 
+{
+	"key": "d",
+	"code": "KeyD",
+	"metaKey": true,
+	"charCode": 100,
+	"keyCode": 100,
+	"which": 100
+});
 
-function sendKeyboardCommand() {
-  document.dispatchEvent(keydownEvent)
+function sendKeyboardCommand() 
+{
+	document.dispatchEvent(keydownEvent);
 }
