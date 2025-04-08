c0a21db0d2526603c14dbe09f7df2bab7d35a7cf
unknown <tomguerineaupro@gmail.com>
2025-04-08 11:57:25 +0200

    update selector

--- ext/js/gathermute.js
+++ ext/js/gathermute.js
@@ -2,8 +2,7 @@
 //debugger;
 
 /// A "Simple" print function, using the Message system.
-function print(message)
-{
+function print(message) {
 	/// Uncomment this to enable printing.
 	//chrome.runtime.sendMessage({log: message});
 }
@@ -14,64 +13,61 @@ function print(message)
 /// 	Muted: class="css-oau38", 
 /// 	Muted-hover: class="css-1o2pj3l"
 /// This method searches for the mute button by class
-const MUTE_BUTTON = 'button.css-15e33lp, button.css-oau38, button.css-1o2pj3l, button.css-434t8s'
+const MUTE_BUTTON = 'button.css-15e33lp, button.css-oau38, button.css-1o2pj3l, button.css-434t8s, button.css-6lehkn'
 
 var mBtn = null;
-function getMuteButton()
-{
+function getMuteButton() {
 	/// isConnected is automatically updated if mBtn is no more connected to any DOM element
-	if(mBtn != null && mBtn.isConnected)
+	if (mBtn != null && mBtn.isConnected)
 		return mBtn;
-	
+
 	mBtn = null;
-	
+
 	var queryList = document.querySelectorAll(MUTE_BUTTON);
 	/// 2 because mute and camera use the same classes. If we have only 1, we dont know which of them it is.
-	if(queryList.length == 2) 
+	if (queryList.length == 2)
 		mBtn = queryList[0];
-	
-	if(!mBtn)
-	{
+
+	if (!mBtn) {
 		/// In some gather functions, like entering / exiting screen sharing the mute button is destroyed and rebuilt
 		//print("[getMuteButton] Could not get mute button.\n" +
 		//"Probably the layout of the page or button class has changed.\n" +
 		//"We will now waitForMuteButton() again.");
-		
+
 		updateMuted(null);
 		waitForMuteButton();
 	}
-	
+
 	return mBtn;
 }
 
 /// Check actual mute state in Gather button.
 /// This uses the class definitions Listed before.
 /// If these values change, we will have to adjust them here.
-function isMuted() 
-{
-	var muteButton = getMuteButton();
-	if(!muteButton)
-		return null;
-	
-	/// Considering anyting but Active and Active-hover as muted.
-	if(muteButton.className == 'css-15e33lp' || muteButton.className == 'css-434t8s')
+function isMuted() {
+	const muteButton = getMuteButton();
+	if (!muteButton) return null;
+	// Exemple en utilisant classList
+	if (muteButton.classList.contains('css-6lehkn')) {
+		return true;
+	} else if (muteButton.classList.contains('css-8hbt4k')) {
 		return false;
-		
-	return true;
+	}
+	// Sinon, inspectez d'autres indicateurs (comme le style ou des attributs aria)
+	return null;
 }
 
 /// Acctually updates internal state and mute button icon
 var muted = null;
-function updateMuted(newValue)
-{
-	if(muted === newValue)
+function updateMuted(newValue) {
+	if (muted === newValue)
 		return;
-	
+
 	muted = newValue;
-	
-	if(muted === null)
-		chrome.runtime.sendMessage({ message: 'disconnected'});
-	else if(muted === true)
+
+	if (muted === null)
+		chrome.runtime.sendMessage({ message: 'disconnected' });
+	else if (muted === true)
 		chrome.runtime.sendMessage({ message: 'muted' });
 	else //if(muted === false)
 		chrome.runtime.sendMessage({ message: 'unmuted' });
@@ -80,45 +76,39 @@ function updateMuted(newValue)
 /// This Method awaits for the initial loading, until the actual Gather
 /// Space is loaded so we can start searching for our mute button
 const expectedBodyChildNodesCount = 7;
-function watchBodyClass() 
-{
-	const bodyClassObserver = new MutationObserver((mutations) =>
-	{
-		if(mutations[0].target.childNodes.length == expectedBodyChildNodesCount)
+function watchBodyClass() {
+	const bodyClassObserver = new MutationObserver((mutations) => {
+		if (mutations[0].target.childNodes.length == expectedBodyChildNodesCount)
 			waitForMuteButton();
 	});
-	
-	bodyClassObserver.observe(document.querySelector('body'), 
-	{
-		childList: true
-	});
+
+	bodyClassObserver.observe(document.querySelector('body'),
+		{
+			childList: true
+		});
 }
 watchBodyClass();
 
-const waitUntilElementExists = (MAX_TIME = 10000) => 
-{
+const waitUntilElementExists = (MAX_TIME = 10000) => {
 	let timeout = 0;
 
-	const waitForContainerElement = (resolve, reject) => 
-	{
+	const waitForContainerElement = (resolve, reject) => {
 		var muteButton = getMuteButton();
-		
+
 		timeout += 200;
-		
-		if (timeout >= MAX_TIME)
-		{
+
+		if (timeout >= MAX_TIME) {
 			print("[waitUntilElementExists] ERROR: Element not found. This is not expected.");
 			reject('Element not found');
 		}
 
-		if (!muteButton || muteButton.length === 0) 
+		if (!muteButton || muteButton.length === 0)
 			setTimeout(waitForContainerElement.bind(this, resolve, reject), 200);
-		else 
+		else
 			resolve(muteButton);
 	}
 
-	return new Promise((resolve, reject) => 
-	{
+	return new Promise((resolve, reject) => {
 		waitForContainerElement(resolve, reject);
 	});
 }
@@ -126,103 +116,90 @@ const waitUntilElementExists = (MAX_TIME = 10000) =>
 /// This is used by watchBodyClass to search for mute button when
 /// Gather space id fully loaded.
 var waitingForMuteButton = false;
-function waitForMuteButton()
-{
-	if (waitingForMuteButton) 
+function waitForMuteButton() {
+	if (waitingForMuteButton)
 		return;
 
 	waitingForMuteButton = true;
-	waitUntilElementExists().then((element) => 
-	{
+	waitUntilElementExists().then((element) => {
 		waitingForMuteButton = false;
 		updateMuted(isMuted());
 		watchIsMuted(element);
-    })
-    .catch((error) => 
-	{
-		chrome.runtime.sendMessage({ message: 'disconnected' });
-    });
+	})
+		.catch((error) => {
+			chrome.runtime.sendMessage({ message: 'disconnected' });
+		});
 }
 
 /// This Method sets an observer in the actual mute button,
 /// So we can reflect changes in our internal state (and mute button icon) in realtime. 
 var isMutedObserver = null;
-function watchIsMuted(element) 
-{
-	if (isMutedObserver) 
+function watchIsMuted(element) {
+	if (isMutedObserver)
 		isMutedObserver.disconnect();
 
-	isMutedObserver = new MutationObserver((mutations) => 
-	{
+	isMutedObserver = new MutationObserver((mutations) => {
 		updateMuted(isMuted());
 	});
-	
-	isMutedObserver.observe(element, 
-	{
-		attributes: true,
-		attributeFilter: ['class'],
-		attributeOldValue: true
-	});
+
+	isMutedObserver.observe(element,
+		{
+			attributes: true,
+			attributeFilter: ['class'],
+			attributeOldValue: true
+		});
 }
 
 /// Attempt to return to "disconected" state.
 /// TODO: This is not working properly yet =/
-window.onbeforeunload = (event) => 
-{
-	try 
-	{
+window.onbeforeunload = (event) => {
+	try {
 		chrome.runtime.sendMessage({ message: 'disconnected' });
-	} 
-	catch (error) 
-	{
+	}
+	catch (error) {
 		print("[window.onbeforeunload] ERROR: " + error);
 	}
 }
 
 /// Acctually change mute state in gather
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => 
-{
-    muted = isMuted();
-	
-	if(!request || !request.command)
-	{
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	muted = isMuted();
+
+	if (!request || !request.command) {
 		print("[chrome.runtime.onMessage.addListener] Invalid request");
 		return;
 	}
-	
+
 	//print("[chrome.runtime.onMessage.addListener] request.command === " + request.command);
-	
-	switch(request.command)
-	{
+
+	switch (request.command) {
 		case 'toggle_mute':
 			isPtt = false;
 			muted = !muted;
 			sendBtnClickCommand();
-		break;
+			break;
 		case 'mute':
 			isPtt = false;
-			if (!muted) 
-			{
+			if (!muted) {
 				muted = true;
 				sendBtnClickCommand();
 			}
-		break;
+			break;
 		case 'unmute':
 			isPtt = false;
-			if (muted) 
-			{
+			if (muted) {
 				muted = false;
 				sendBtnClickCommand();
 			}
-		break;
+			break;
 		case 'ptt':
 			processPTT();
-		break;
+			break;
 	}
-	
-	if(muted === null)
+
+	if (muted === null)
 		sendResponse({ message: 'disconnected' });
-	else if(muted === true)
+	else if (muted === true)
 		sendResponse({ message: 'muted' });
 	else //if(muted === false)
 		sendResponse({ message: 'unmuted' });
@@ -232,41 +209,36 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
 var isPtt = false;
 var pttTimeoutHandle = null;
 var pttDelayTimeInMs = 1000;
-var pttResetTimerMs  = 500;
+var pttResetTimerMs = 500;
 var pttStartTime = 0;
-function processPTT()
-{
-	if (!isPtt)
-	{
+function processPTT() {
+	if (!isPtt) {
 		isPtt = true;
-	
-		if (muted) 
-		{
+
+		if (muted) {
 			updateMuted(false);
 			sendBtnClickCommand();
 		}
 	}
-	
+
 	/// Check time since last timer, for performance?
-	if(Date.now() - pttStartTime < pttResetTimerMs)
+	if (Date.now() - pttStartTime < pttResetTimerMs)
 		return;
-	
+
 	/// Always reset timer here
 	pttStartTime = Date.now();
 	clearTimeout(pttTimeoutHandle);
 	pttTimeoutHandle = setTimeout(onPttExpired, pttDelayTimeInMs);
 }
 
-function onPttExpired()
-{
-	if(!isPtt)
+function onPttExpired() {
+	if (!isPtt)
 		return;
-		
+
 	isPtt = false;
 	pttStartTime = 0;
-	
-	if (!muted)
-	{
+
+	if (!muted) {
 		updateMuted(true);
 		sendBtnClickCommand();
 	}
@@ -275,27 +247,25 @@ function onPttExpired()
 ///////// Events to control gather button
 /// KeyBoard Event - Ctrl+Shift+A 
 /// This is the shortcut gather uses for muting, but for some reason, sending it has no effect
-function sendKeyboardCommand() 
-{
+function sendKeyboardCommand() {
 	document.dispatchEvent(new KeyboardEvent('keypress',
-	{
-		"key": "A",
-		"code": "KeyA",
-		"ctrlKey": true,
-		"shiftKey": true,
-		//"metaKey": true,
-		"charCode": 97,
-		"keyCode": 97,
-		"which": 97,
-		"isTrusted": true
-	}));
+		{
+			"key": "A",
+			"code": "KeyA",
+			"ctrlKey": true,
+			"shiftKey": true,
+			//"metaKey": true,
+			"charCode": 97,
+			"keyCode": 97,
+			"which": 97,
+			"isTrusted": true
+		}));
 }
 
 /// Button click is still working =) ... for now.
-function sendBtnClickCommand()
-{
+function sendBtnClickCommand() {
 	var muteButton = getMuteButton();
-	if(muteButton)
+	if (muteButton)
 		muteButton.click();
 }
 
